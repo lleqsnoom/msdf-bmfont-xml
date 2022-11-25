f5127b245df9e559312e1bac8b5cf28eff11870a
Tomasz Kwiatek <lleqsnoom@gmail.com>
2022-11-25 21:10:52 +0100

    Add linux arm64 support

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const controlChars = ['\n', '\r', '\t'];
 const binaryLookup = {
   darwin: 'msdfgen.osx',
   win32: 'msdfgen.exe',
-  linux: 'msdfgen.linux'
+  linux: 'msdfgen.linux',
+  linux_arm64: 'msdfgen.linux'
 };
 
 module.exports = generateBMFont;
@@ -45,13 +46,13 @@ module.exports = generateBMFont;
  * @param {function(string, Array.<Object>, Object)} callback - Callback funtion(err, textures, font)
  *
  */
-function generateBMFont (fontPath, opt, callback) {
+function generateBMFont(fontPath, opt, callback) {
   if (typeof opt === 'function') {
     callback = opt;
     opt = {};
   }
 
-  const binName = binaryLookup[process.platform];
+  const binName = process.arch === "arm64" ? binaryLookup[`${process.platform}_${process.arch}`] : binaryLookup[process.platform];
 
   assert.ok(binName, `No msdfgen binary for platform ${process.platform}.`);
   assert.ok(fontPath, 'must specify a font path');
@@ -131,7 +132,7 @@ function generateBMFont (fontPath, opt, callback) {
 
   const fontface = typeof fontPath === 'string' ? path.basename(fontPath, path.extname(fontPath)) : filename;
 
-  if(!filename) {
+  if (!filename) {
     filename = fontface;
     console.log(`Use font-face as filename : ${filename}`);
   } else {
@@ -153,7 +154,7 @@ function generateBMFont (fontPath, opt, callback) {
   bar = new ProgressBar.Bar({
     format: "Generating {percentage}%|{bar}| ({value}/{total}) {duration}s",
     clearOnComplete: true
-  }, ProgressBar.Presets.shades_classic); 
+  }, ProgressBar.Presets.shades_classic);
   bar.start(charset.length, 0);
 
   mapLimit(charset, 15, (char, cb) => {
@@ -182,9 +183,9 @@ function generateBMFont (fontPath, opt, callback) {
       let texname = "";
       let fillColor = fieldType === "msdf" ? 0x000000ff : 0x00000000;
       let img = new Jimp(bin.width, bin.height, fillColor);
-      if (index > pages.length - 1) { 
+      if (index > pages.length - 1) {
         if (packer.bins.length > 1) texname = `${filename}.${index}`;
-        else texname = filename; 
+        else texname = filename;
         pages.push(`${texname}.png`);
       } else {
         texname = path.basename(pages[index], path.extname(pages[index]));
@@ -219,7 +220,7 @@ function generateBMFont (fontPath, opt, callback) {
       const buffer = await img.getBufferAsync(Jimp.MIME_PNG);
       let tex = {
         filename: path.join(fontDir, texname),
-        texture: buffer 
+        texture: buffer
       }
       if (debug) tex.svg = svg;
       return tex;
@@ -276,7 +277,7 @@ function generateBMFont (fontPath, opt, callback) {
       },
       kernings: kernings
     };
-    if(roundDecimal !== null) utils.roundAllValue(fontData, roundDecimal, true);
+    if (roundDecimal !== null) utils.roundAllValue(fontData, roundDecimal, true);
     let fontFile = {};
     const ext = outputType === "json" ? `.json` : `.fnt`;
     fontFile.filename = path.join(fontDir, fontface + ext);
@@ -285,7 +286,7 @@ function generateBMFont (fontPath, opt, callback) {
     // Store pages name and available packer freeRects in settings
     settings.pages = pages;
     settings.packer = {};
-    settings.packer.bins = packer.save(); 
+    settings.packer.bins = packer.save();
     fontFile.settings = settings;
 
     console.log("\nGeneration complete!\n");
@@ -293,8 +294,8 @@ function generateBMFont (fontPath, opt, callback) {
   });
 }
 
-function generateImage (opt, callback) {
-  const {binaryPath, font, char, fontSize, fieldType, distanceRange, roundDecimal, debug, tolerance} = opt;
+function generateImage(opt, callback) {
+  const { binaryPath, font, char, fontSize, fieldType, distanceRange, roundDecimal, debug, tolerance } = opt;
   const glyph = font.charToGlyph(char);
   const commands = glyph.getPath(0, 0, fontSize).commands;
   let contours = [];
@@ -365,7 +366,7 @@ function generateImage (opt, callback) {
       height = 0;
     } else {
       const buffer = new Uint8ClampedArray(pixels);
-      imageData = new Jimp({data: buffer, width: width, height: height});
+      imageData = new Jimp({ data: buffer, width: width, height: height });
     }
     const container = {
       data: {
@@ -387,7 +388,7 @@ function generateImage (opt, callback) {
     };
     callback(null, container);
   });
-  
+
   subproc.stdin.write(shapeDesc);
   subproc.stdin.write('\n');
   subproc.stdin.destroy();
